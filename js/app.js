da99671a68869229438d0670ed5054034b2d6651
Yan Hong <onlyhongyan@gmail.com>
2017-08-13 12:04:19 -0700

    add edit and delete student

---
+++ js/app.js
@@ -0,0 +1,52 @@
+var myapp = angular.module('student', []);
+var host = 'http://student.webdxd.com/api/';
+
+myapp.controller('studentController', function($http, $scope) {
+
+    $scope.students = [];
+
+    $http.get(host + 'student').success(function(response) {
+        $scope.students = response
+    })
+
+    $scope.getStudentDetail = function(sid) {
+        $http.get(host + 'student/' + sid).success(function(response) {
+            $scope.clickedStudent = response
+        })
+    }
+
+    $scope.showEditForm = function() {
+        $scope.newStudent = $scope.clickedStudent
+        $scope.clickedStudent = undefined
+    }
+
+    $scope.createNewStudent = function() {
+        $http.post(host + 'student', $scope.newStudent).success(function(response) {
+
+
+            if (response.isNew) {
+                $scope.students.push(response.data)
+            }
+
+            $scope.clickedStudent = response.data
+            $scope.newStudent = undefined
+        })
+    }
+
+    $scope.clearClickedStudent = function () {
+        $scope.clickedStudent = undefined
+        $scope.newStudent = undefined
+    }
+
+    $scope.deleteStudent = function(sid) {
+        $http.delete(host + 'student/' + sid).success(function(response) {
+            for (var i = 0; i < $scope.students.length; i++) {
+                if ($scope.students[i]._id === response._id) {
+                    $scope.students.splice(i, 1);
+                    $scope.clickedStudent = undefined
+                }
+            }
+        })
+    }
+
+})
