ae97f4df18e33b60c7677736d7ffb6bd92e3083a
Yan Hong <onlyhongyan@gmail.com>
2017-08-13 16:04:51 -0700

    update angularjs student files

--- js/app.js
+++ js/app.js
@@ -1,52 +1,60 @@
-var myapp = angular.module('student', []);
-var host = 'http://student.webdxd.com/api/';
+var myapp = angular.module('student', [])
+var host = 'http://student.webdxd.com/api/'
 
-myapp.controller('studentController', function($http, $scope) {
+myapp.controller('studentController', function($http, $scope){
 
-    $scope.students = [];
+	$scope.students = []
 
-    $http.get(host + 'student').success(function(response) {
-        $scope.students = response
-    })
+	$http.get(host + 'student').success(function(response){
+		$scope.students = response
+	})
 
-    $scope.getStudentDetail = function(sid) {
-        $http.get(host + 'student/' + sid).success(function(response) {
-            $scope.clickedStudent = response
-        })
-    }
+	$scope.getStudentDetail = function(sid) {
 
-    $scope.showEditForm = function() {
-        $scope.newStudent = $scope.clickedStudent
-        $scope.clickedStudent = undefined
-    }
+		$http.get(host + 'student/' + sid).success(function(response) {
+			$scope.clickedStudent = response
+		})
 
-    $scope.createNewStudent = function() {
-        $http.post(host + 'student', $scope.newStudent).success(function(response) {
+	}
+
+	$scope.createNewStudent = function() {
+
+		$http.post(host + 'student', $scope.newStudent).success(function(response) {
+			if (response.isNew) {
+				$scope.students.push(response.data)
+				
+			} else {
+				for (var i = 0; i < $scope.students.length; i++) {
+					if ($scope.students[i]._id === response.data._id) {
+						$scope.students[i] = response.data
+						$scope.clickedStudent = response.data
+					}
+				}
+			}
+
+			$scope.newStudent = undefined
+			
+		})
+
+	}
 
 
-            if (response.isNew) {
-                $scope.students.push(response.data)
-            }
+	$scope.showEditForm = function() {
+		$scope.newStudent = $scope.clickedStudent
+		$scope.clickedStudent = undefined
+	}
 
-            $scope.clickedStudent = response.data
-            $scope.newStudent = undefined
-        })
-    }
+	$scope.deleteStudent = function(sid) {
+		$http.delete(host + 'student/' + sid).success(function(response) {
+			for (var i = 0; i < $scope.students.length; i++) {
+					if ($scope.students[i]._id === response._id) {
+						$scope.students.splice(i, 1)
+						$scope.clickedStudent = undefined
+					}
+				}
+		})
+	}
 
-    $scope.clearClickedStudent = function () {
-        $scope.clickedStudent = undefined
-        $scope.newStudent = undefined
-    }
-
-    $scope.deleteStudent = function(sid) {
-        $http.delete(host + 'student/' + sid).success(function(response) {
-            for (var i = 0; i < $scope.students.length; i++) {
-                if ($scope.students[i]._id === response._id) {
-                    $scope.students.splice(i, 1);
-                    $scope.clickedStudent = undefined
-                }
-            }
-        })
-    }
+	
 
 })
